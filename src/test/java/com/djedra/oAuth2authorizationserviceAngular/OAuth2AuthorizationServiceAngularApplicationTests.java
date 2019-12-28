cc709393da0b4c26e5e719633d4e05fa399750ca
SanDiegos <52674560+SanDiegos@users.noreply.github.com>
2019-12-28 14:23:17 +0100

    Base

---
+++ src/test/java/com/djedra/oAuth2authorizationserviceAngular/OAuth2AuthorizationServiceAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djedra.oAuth2authorizationserviceAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2AuthorizationServiceAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
